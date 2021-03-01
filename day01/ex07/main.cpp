b426e0a937103ad2a9c91a949a3cc566b63f785b
jdel-ros <juan.burn@hotmail.fr>
2021-03-01 18:27:14 +0100

    day01_OK

--- day01/ex07/main.cpp
+++ day01/ex07/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdel-ros <jdel-ros@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: juan <juan@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 14:44:45 by jdel-ros          #+#    #+#             */
-/*   Updated: 2021/03/01 16:00:02 by jdel-ros         ###   ########lyon.fr   */
+/*   Updated: 2021/03/01 18:25:38 by juan             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,42 +16,44 @@
 
 int			main(int argc, char **argv)
 {
-	(void)argc;
-	// if (argc != 4)
-	// {
-	// 	std::cout << "don't troll dude" << std::endl;
-	// 	return (0);
-	// }
-	// if (argv[2] == NULL || argv[3] == NULL || argv[4] == NULL)
-	// {
-	// 	std::cout << "don't troll dude" << std::endl;
-	// 	return (0);
-	// }
-	std::string buff;
+	if (argc != 4)
+	{
+		std::cout << "don't troll dude" << std::endl;
+		return (0);
+	}
+	if (argv[1] == NULL || argv[2] == NULL || argv[3] == NULL)
+	{
+		std::cout << "don't troll dude" << std::endl;
+		return (0);
+	}
+
+	std::string newFile;
 	std::string buf;
-	buff = argv[1];
-	buff.append(".replace");
+	newFile = argv[1];
+	newFile.append(".replace");
 	std::ifstream	ifs(argv[1]);
-	std::ofstream ofs(buff);
+	std::ofstream ofs(newFile);
 
 	while (std::getline(ifs, buf))
 	{
 		std::cout << buf << std::endl;
-		size_t	pos(0);
-		std::string		resul;
+		size_t	pos = 0;
+		// std::string		resul;
 
 		while (!buf.empty())
 		{
 			if ((pos = buf.find(argv[2], pos)) != std::string::npos)
 			{
 				std::cout << pos << std::endl;
-				buf.replace(pos, std::string(argv[3]).size(), argv[3]);
+				buf.replace(pos, std::string(argv[2]).size(), argv[3]);
 			}
 			ofs << buf.substr(0, pos);
 			buf.erase(0, pos);
+			pos = 0;
+			std::cout << "DEBUG = " << buf << std::endl;
 		}
-			if (!ifs.eof())
-				ofs << std::endl;
+		if (!ifs.eof())
+			ofs << std::endl;
 	}
 	return (0);
 }
